93151c5d64e57c405a6bb3d4fe8147db3db6b837
Shankar Dhanasekaran <shankardevy@gmail.com>
2017-07-08 20:53:32 +0530

    Modify Ticket CRUD resource as needed

--- lib/mango/web/router.ex
+++ lib/mango/web/router.ex
@@ -42,7 +42,7 @@ defmodule Mango.Web.Router do
     get "/logout", SessionController, :delete
     get "/checkout", CheckoutController, :edit
     put "/checkout/confirm", CheckoutController, :update
-    resources "/tickets", TicketController
+    resources "/tickets", TicketController, except: [:edit, :update, :delete]
   end
 
 end
