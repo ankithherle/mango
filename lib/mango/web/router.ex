c90d8fdb1dddc56c3f3242d11edf59fed40d85e0
Shankar Dhanasekaran <shankardevy@gmail.com>
2017-07-08 22:19:27 +0530

    Manage orders

--- lib/mango/web/router.ex
+++ lib/mango/web/router.ex
@@ -65,6 +65,7 @@ defmodule Mango.Web.Router do
 
     # Add all routes that require admin authentication
     resources "/users", UserController
+    resources "orders", OrderController, only: [:index, :show]
     get "/logout", SessionController, :delete
   end
 
---
+++ lib/mango/web/views/admin/order_view.ex
@@ -0,0 +1,3 @@
+defmodule Mango.Web.Admin.OrderView do
+  use Mango.Web, :view
+end
